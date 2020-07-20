035fbb4070e0abb2239550600d0feffde0c2c11a
tt.huy.dang <tt.huy.dang@tomosia.com>
2020-07-20 16:43:09 +0700

    xong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # Tell the UserMailer to send a welcome email after save
+      UserMailer.welcome_email(@user).deliver_later
+
       redirect_to @user
     else
       render 'new'
